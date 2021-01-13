1e3ec968c5afda414eeeeee33f30dd3f52410d13
Wayne Ferrao <wayne.ferrao93@gmail.com>
2021-01-13 16:23:49 -0500

    Project restructure

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-require('dotenv').config();
+require('dotenv/types').config();
 let cors = require('cors');
 const app = express();
 let axios = require('axios');
@@ -21,15 +21,14 @@ app.get('/getMovies', (req, res)=> {
 
 if (process.env.NODE_ENV === 'production') {
     // Express will serve up production assets
-    app.use(express.static('build'));
+    app.use(express.static('client/build'));
 
     // Express serve up index.html file if it doesn't recognize route
     const path = require('path');
     app.get('/*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-
+        res.sendFile(path.resolve(__dirname, 'client','build', 'index.html'));
     });
 
 }
 const PORT = process.env.PORT || 5000;
-app.listen(5000);
+app.listen(PORT);
